5d67cef0acdadd3b7ccafdeafd6d8231c54a797e
MAHESHKUMAR A <120610732+aingrose@users.noreply.github.com>
2023-11-27 07:47:42 +0530

    Add files via upload

---
+++ Data-json.js
@@ -0,0 +1,16 @@
+var Data ={
+    "name": "Maheshkumar",
+    "title": "Fsd Developer",
+    "contact": {
+      "email": "maheshrdrv@gmail.com",
+      "phone": "+1234567890",
+      "location": "Coimbatore,"
+    },
+    "education": [
+      {
+        "degree": "Bachelor of Electrical and electronics Engineering",
+        "school": "KPR institute of engineering and technology",
+        "year": "2023"
+      }
+    ]
+}
